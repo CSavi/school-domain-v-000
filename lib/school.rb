a713acca9969149b4f2088564f598ed261daee4d
Carrie Savinell <carrielane89@gmail.com>
2018-05-18 13:47:18 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School    #referred to as the model 
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end   
+  
+  def add_student(name, grade)
+    if !roster[grade] = nil 
+      
+      
+  
+
+end   
